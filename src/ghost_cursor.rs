11d23690b25ae371a9e8a84544feb4f91503beb6
konsumlamm <konsumlamm@gmail.com>
2022-05-11 18:08:30 +0200

    Clarify wording

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -17,7 +17,7 @@
 //! This is the crux of the issue, and the most likely place for unsoundness in the whole scheme.
 //!
 //! Let us start by a broken example to better understand what we are looking for. Let us imagine a simple doubly linked
-//! list data structure where each node has an optional previous and next field to point to the previous and next node,
+//! list data structure where each node has two optional fields, a previous and a next field, to point to the previous and next node,
 //! respectively.
 //!
 //! Imagine the following set-up with 2 nodes `a` and `b`:
